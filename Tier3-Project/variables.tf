eff644bc33e0193007181fbbdad55748c85d125b
Ramya <85498681+Prasannakurapati9@users.noreply.github.com>
2026-09-22 23:08:39 +0530

    Add files via upload

---
+++ Tier3-Project/variables.tf
@@ -0,0 +1,40 @@
+variable "rg_name" {
+  type = string
+  description = "used to name the resource group"
+}
+
+variable "location" {
+  type = string
+  description = "used to name the location"
+}
+
+variable "vnet_name" {
+  type = string
+  description = "used to name the location"
+}
+
+variable "Snet01" {
+  type = string
+  description = "used to name the Websubnet"
+}
+
+variable "Snet02" {
+  type = string
+  description = "used to name the Appsubnet"
+}
+
+variable "Snet03" {
+  type = string
+  description = "used to name the DBsubnet"
+}
+variable "Snet04" {
+  type = string
+  description = "used to name the Bastionsubnet"
+}
+
+variable "Snet05" {
+  type = string
+  description = "used to name the Bastionsubnet"
+}
+
+
